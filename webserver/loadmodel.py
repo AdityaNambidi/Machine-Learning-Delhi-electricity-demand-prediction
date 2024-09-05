f9b416a906636402c66c3cfd6c5414fae3546dd6
AdityaNambidi <aditya200513@gmail.com>
2024-09-05 16:28:27 +0530

    'c2'

---
+++ webserver/loadmodel.py
@@ -0,0 +1,4 @@
+import joblib
+
+model = joblib.load("rf_model.pkl")
+scaler = joblib.load("rf_scaler.pkl")
--- webserver/main.py
+++ webserver/main.py
@@ -1,18 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask , render_template,request,jsonify
+import datetime
+from loadmodel import scaler, model
+import requests
+import pandas as pd
 
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Define a route for the home page
-@app.route('/')
+@app.route("/")
 def home():
-    return "hello"
+    return render_template('index.html')
 
-@app.route("/specifictime", methods=["GET", "POST"])
+@app.route('/specifictime', methods=['POST'])
 def specific_time():
-    return 0
+    data = request.get_json()
+    selected_date = data['date']
+    selected_time = data['time']
 
+    datetime_input = datetime.datetime.strptime(f"{selected_date} {selected_time}", '%Y-%m-%d %H:%M')
+    
+    weather_url = str("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/28.7041,77.1025/" + str(selected_date) + "T" + str(datetime_input.hour) + ":00:00?key=FZFCJYLLJBGX7MFWSTXUSNE29")
+    res = requests.get(weather_url)
+    wdata = res.json()
 
-# Start the Flask server
-if __name__ == '__main__':
+    april19 = 2833.3958333333335
+
+    inputdata = pd.DataFrame({
+        'month' : [datetime_input.month],
+        'hour' : [datetime_input.hour],
+        'temp' : [wdata['days'][0]['hours'][datetime_input.hour]['temp']],
+        'wind_speed' : [wdata['days'][0]['hours'][datetime_input.hour]['windspeed']],
+        'preasure': [wdata['days'][0]['hours'][datetime_input.hour]['pressure']],
+        'prev_avg_e': [april19]
+
+    })
+
+    inputdata[['month', 'hour', 'temp', 'wind_speed', 'preasure', 'prev_avg_e']] = scaler.transform(inputdata[['month', 'hour', 'temp', 'wind_speed', 'preasure', 'prev_avg_e']])
+    result = model.predict(inputdata)[0]
+    print(result)
+    
+    return jsonify({'result': result})
+
+if __name__ == "__main__":
     app.run(debug=True)

149dd010897219d61795e343ef69744d1d1504e9
AdityaNambidi <aditya200513@gmail.com>
2024-09-05 18:54:44 +0530

    c3

--- webserver/main.py
+++ webserver/main.py
@@ -3,8 +3,10 @@ import datetime
 from loadmodel import scaler, model
 import requests
 import pandas as pd
+import os
 
 app = Flask(__name__)
+APIKEY = "ZB4Y226LCUMVPKCCYVZS63SEG"
 
 @app.route("/")
 def home():
@@ -18,7 +20,7 @@ def specific_time():
 
     datetime_input = datetime.datetime.strptime(f"{selected_date} {selected_time}", '%Y-%m-%d %H:%M')
     
-    weather_url = str("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/28.7041,77.1025/" + str(selected_date) + "T" + str(datetime_input.hour) + ":00:00?key=FZFCJYLLJBGX7MFWSTXUSNE29")
+    weather_url = str("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/28.7041,77.1025/" + str(selected_date) + "T" + str(datetime_input.hour) + ":00:00?key=" + APIKEY)
     res = requests.get(weather_url)
     wdata = res.json()
 
@@ -39,6 +41,39 @@ def specific_time():
     print(result)
     
     return jsonify({'result': result})
+
+
+@app.route('/rangetime', methods=['POST'])
+def range_time():
+    return 0
+
+
+@app.route('/nexthour')
+def next_hour():
+    now = datetime.datetime.now()
+
+    weather_url = str("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/28.7041,77.1025/" + str(now.date()) + "T" + str(now.hour + 1) + ":00:00?key=" + APIKEY)
+    res = requests.get(weather_url)
+    wdata = res.json()
+
+    april19 = 2833.3958333333335
+
+    inputdata = pd.DataFrame({
+        'month' : [now.month],
+        'hour' : [now.hour + 1],
+        'temp' : [wdata['days'][0]['hours'][now.hour]['temp']],
+        'wind_speed' : [wdata['days'][0]['hours'][now.hour + 1]['windspeed']],
+        'preasure': [wdata['days'][0]['hours'][now.hour + 1]['pressure']],
+        'prev_avg_e': [april19]
+
+    })
+
+    inputdata[['month', 'hour', 'temp', 'wind_speed', 'preasure', 'prev_avg_e']] = scaler.transform(inputdata[['month', 'hour', 'temp', 'wind_speed', 'preasure', 'prev_avg_e']])
+    result = model.predict(inputdata)[0]
+    print(result)
+
+    return jsonify({'result': result})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
